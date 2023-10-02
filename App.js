f79c6bdd07bb45749ab97bed2816b4667a3d48f1
jonx1183 <jonx1183@stud.kea.dk>
2023-10-02 15:39:35 +0200

    Location?

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, {Marker}  from 'react-native-maps';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import * as Location from 'expo-location';
 export default function App() {
 
 const [markers, setMarkers] = useState([])  
@@ -13,6 +14,40 @@ const [region, setRegion] = useState({
 
 })
 
+const mapView = useRef(null)
+const locationSubscription = useRef(null)
+
+useEffect(() => {
+  async function startListening(){
+    let { status} = await Location.requestForegroundPermissionsAsync()
+    if(status !=='granted'){
+      alert("Can not access lacation")
+      return
+    }
+     locationSubscription.current = await Location.watchPositionAsync({
+      distanceInterval: 100,
+      accuracy: Location.Accuracy.High
+     }, (lokation) =>{
+      const newRegion = {
+        latitude:lokation.coords.latitude,
+        longitude:lokation.coords.longitude,
+        latitudeDelta:20,
+        longitudeDelta:20
+      }
+      setRegion(newRegion)
+      if(mapView.current){
+        mapView.current.animateToRegion(newRegion)
+      }
+     })
+  }
+  startListening()
+  return () => {
+    if(locationSubscription.current){
+      locationSubscription.current.remove()
+    }
+  }
+}, [])
+
 function addMarker(data){
   const {latitude, longitude} = data.nativeEvent.coordinate
   const newMarker = {
@@ -21,6 +56,10 @@ function addMarker(data){
     title: "Nepu nepu!"
   }
   setMarkers([...markers, newMarker])
+}
+
+function onMarkerPressed(text){
+  alert("You pressed: " + text)
 }
 
   return (
@@ -35,6 +74,7 @@ function addMarker(data){
              coordinate={marker.coordinate}
              key={marker.key}
              title={marker.title}
+             onPress={() => onMarkerPressed(marker.title)}
           />
         ))
 
