b029237c752d28e4ec46942dba259916d56e6c26
jonx1183 <jonx1183@stud.kea.dk>
2023-10-02 14:55:28 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import MapView, {Marker}  from 'react-native-maps';
+import { useState } from 'react';
 export default function App() {
+
+const [markers, setMarkers] = useState([])  
+const [region, setRegion] = useState({
+  latitude:55,
+  longitude:12,
+  latitudeDelta:20,
+  longitudeDelta:20
+
+})
+
+function addMarker(data){
+  const {latitude, longitude} = data.nativeEvent.coordinate
+  const newMarker = {
+    coordinate: {latitude, longitude},
+    key: data.timeStamp,
+    title: "Nepu nepu!"
+  }
+  setMarkers([...markers, newMarker])
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView 
+      style={styles.map}
+      region={region}
+      onLongPress={addMarker}
+      >
+        {markers.map(marker => (
+          <Marker
+             coordinate={marker.coordinate}
+             key={marker.key}
+             title={marker.title}
+          />
+        ))
+
+        }
+        
+
+      </MapView>
+  
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  map: {
+    width: '100%',
+    height: '100%'
   },
 });
